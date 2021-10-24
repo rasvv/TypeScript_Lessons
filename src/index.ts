30a506dd1659c050480a9bc63524482a0d6a5388
Валерий <rasvv@mail.ru>
2021-10-24 20:21:22 +0300

    Начало ДЗ2

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,38 @@
 import { renderSearchFormBlock } from './search-form.js'
-import { renderSearchStubBlock } from './search-results.js'
+import { renderSearchStubBlock, renderSearchResultsBlock } from './search-results.js'
 import { renderUserBlock } from './user.js'
 import { renderToast } from './lib.js'
+
+const localStorage = {
+  user: {
+    'username': 'RVV',
+    'avatarUrl': '/img/avatar.png'
+  },
+  'favoritesAmount': 5
+}
 
 const nowDate = new Date()
 const inDate = new Date(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDate() + 2).toISOString().slice(0,10)
 const outDate = new Date(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDate() + 4).toISOString().slice(0,10)
 
+const getUserData = () => {
+  return (localStorage.user)
+}
+
+const { username, avatarUrl } = getUserData()
+
+const favoritesAmount = () => {
+  return (localStorage['favoritesAmount'])
+}
+
 window.addEventListener('DOMContentLoaded', () => {
-  renderUserBlock('rvv', '/img/avatar.png', 5)
+  renderUserBlock(username, avatarUrl, favoritesAmount())
   renderSearchFormBlock(inDate, outDate)
-  renderSearchStubBlock()
+  renderSearchResultsBlock()
+  // renderSearchStubBlock()
   renderToast(
     {text: 'Это пример уведомления. Используйте его при необходимости', type: 'success'},
     {name: 'Понял', handler: () => {console.log('Уведомление закрыто')}}
   )
 })
+
