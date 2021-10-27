4a95c554ff2ef583a5081938c11208ed5cd298a3
Валерий <rasvv@mail.ru>
2021-10-27 21:33:04 +0300

    Навести красоту перед сдачей

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,68 @@
-import { renderSearchFormBlock, getSearchFormData } from './search-form.js'
-import { renderSearchStubBlock } from './search-results.js'
+import { renderSearchFormBlock } from './search-form.js'
+import { renderSearchStubBlock, renderSearchResultsTemplateBlock } from './search-results.js'
 import { renderUserBlock } from './user.js'
 import { renderToast } from './lib.js'
+// import JSON from '../db.json'
+
+// import { readFile } from 'fs'
+import { places } from './db.js'
+// const fs = require('fs');
+// import * as dataJSON from '../db.json';
+// const places =  resolveJsonModule('../db.json')
+
+
+export interface resultPlaces {
+  id: number
+  name: string
+  description: string
+  image: string
+  remoteness: number
+  bookedDates: [
+    
+  ],
+  price: number 
+  favorite: boolean 
+}
+
+// export const readerJSON = () => {
+//   readFile('../db.json', 'utf-8', (err, data) => {
+//     if (data) { return data }
+//   })
+// }
+
+let liTemplate = ''
+
+const buildLiTemplate = (place: resultPlaces) => {
+  return `
+  <li class="result">
+  <div class="result-container">
+  	<div class="result-img-container">
+  		<div class="favorites"></div>
+  		<img class="result-img" src=${place.image} alt="">
+  	</div>	
+  	<div class="result-info">
+  		<div class="result-info--header">
+  			<p>${place.name}</p>
+  			<p class="price">${place.price}&#8381;</p>
+  		</div>
+  		<div class="result-info--map"><i class="map-icon"></i> ${place.remoteness}км от вас</div>
+  		<div class="result-info--descr">${place.description}</div>
+  		<div class="result-info--footer">
+  			<div>
+  				<button>Забронировать</button>
+  			</div>
+  		</div>
+  	</div>
+  </div>
+  </li>
+  `
+}
+
+places.forEach((place:resultPlaces) => {
+  liTemplate +=buildLiTemplate(place)
+  // place.
+})
+
 
 const localStorage = {
   user: {
@@ -49,8 +110,9 @@ const favoritesAmount = () => {
 window.addEventListener('DOMContentLoaded', () => {
   renderUserBlock(username, avatarUrl, favoritesAmount())
   renderSearchFormBlock(inDate, outDate, maxPrice)
-  renderSearchStubBlock()
-  if (getSearchFormData()) {
+  // renderSearchStubBlock()
+  renderSearchResultsTemplateBlock(liTemplate)  
+  if (searchString) {
     renderToast(
       {text: `${city}, ${inDate}, ${outDate}, ${maxPrice}`, type: 'success'},
       {name: 'Ясно', handler: () => {console.log('Уведомление закрыто')}}
--- src/search-results.ts
+++ src/search-results.ts
@@ -160,10 +160,89 @@ export function renderSearchResultsJSONBlock () {
           </div>
         </div>
       </li>
+      <li class="result">
+        <div class="result-container">
+          <div class="result-img-container">
+            <div class="favorites active"></div>
+            <img class="result-img" src="./img/result-3.png" alt="">
+          </div>	
+          <div class="result-info">
+            <div class="result-info--header">
+              <p>Solo Sokos Hotel Palace Bridge</p>
+              <p class="price">10500&#8381;</p>
+            </div>
+            <div class="result-info--map"><i class="map-icon"></i> 5.0км от вас</div>
+            <div class="result-info--descr">Отель с wellness-центром расположен на Васильевском острове. Отель отличает наличие спа-центра и центральное местоположение.</div>
+            <div class="result-info--footer">
+              <div>
+                <button>Забронировать</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+      <li class="result">
+        <div class="result-container">
+          <div class="result-img-container">
+            <div class="favorites"></div>
+            <img class="result-img" src="./img/result-4.png" alt="">
+          </div>	
+          <div class="result-info">
+            <div class="result-info--header">
+              <p>Park Inn by Radisson Pulkovskaya Hotel</p>
+              <p class="price">6600&#8381;</p>
+            </div>
+            <div class="result-info--map"><i class="map-icon"></i> 15.3км от вас</div>
+            <div class="result-info--descr">Отель оборудован бизнес-центром, залами для совещаний и конференц-центром находится рядом с площадью Победы и парком Городов-Героев.</div>
+            <div class="result-info--footer">
+              <div>
+                <button>Забронировать</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
     </ul>
     `
   )
+
+  const favoritesImg = document.getElementsByClassName('favorites')
+  Array.from(favoritesImg).forEach(element => {
+    element.addEventListener('click', () => {
+      element.classList.toggle('active')
+    })
+  })
 }
+
+export function renderSearchResultsTemplateBlock (liTemplate) {
+  renderBlock(
+    'search-results-block',
+    `
+    <div class="search-results-header">
+        <p>Результаты поиска</p>
+        <div class="search-results-filter">
+            <span><i class="icon icon-filter"></i> Сортировать:</span>
+            <select>
+                <option selected="">Сначала дешёвые</option>
+                <option selected="">Сначала дорогие</option>
+                <option>Сначала ближе</option>
+            </select>
+        </div>
+    </div>
+    <ul class="results-list">
+      ${liTemplate}
+    </ul>
+    `
+  )
+  
+  const favoritesImg = document.getElementsByClassName('favorites')
+  Array.from(favoritesImg).forEach(element => {
+    element.addEventListener('click', () => {
+      element.classList.toggle('active')
+    })
+  })
+}
+
 
 // const liBlock = `
 // <li class="result">
